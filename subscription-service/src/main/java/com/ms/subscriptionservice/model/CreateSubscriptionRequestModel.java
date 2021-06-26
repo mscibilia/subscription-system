091852044452c6663ab4d9bd7fbfc559477db763
Scibs <mscibilia91@gmail.com>
2021-06-26 23:53:28 +0800

    Fix deserialization to LocalDate.

--- subscription-service/src/main/java/com/ms/subscriptionservice/model/CreateSubscriptionRequestModel.java
+++ subscription-service/src/main/java/com/ms/subscriptionservice/model/CreateSubscriptionRequestModel.java
@@ -1,5 +1,8 @@
 package com.ms.subscriptionservice.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
 import lombok.Data;
 
 import javax.validation.constraints.NotNull;
@@ -15,6 +18,8 @@ public class CreateSubscriptionRequestModel {
     private Boolean consentFlag;
 
     @NotNull(message = "date of birth cannot be null")
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd/MM/yyyy")
     private LocalDate dateOfBirth;
 
     @NotNull(message = "email address cannot be null")
