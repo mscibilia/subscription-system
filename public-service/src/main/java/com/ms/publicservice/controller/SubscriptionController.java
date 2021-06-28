6851d75a2c2a2e3bbe13a35a95da772a7923f061
Scibs <mscibilia91@gmail.com>
2021-06-28 10:56:16 +0800

    Fixed request body validation on public service.

--- public-service/src/main/java/com/ms/publicservice/controller/SubscriptionController.java
+++ public-service/src/main/java/com/ms/publicservice/controller/SubscriptionController.java
@@ -4,15 +4,18 @@ import com.ms.publicservice.model.CreateSubscriptionRequestModel;
 import com.ms.publicservice.model.UpdateSubscriptionRequestModel;
 import com.ms.publicservice.service.SubscriptionService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 
 @RestController
-@Validated
 @RequiredArgsConstructor
 @RequestMapping(path = "/subscriptions")
 public class SubscriptionController {
@@ -20,8 +23,10 @@ public class SubscriptionController {
     private final SubscriptionService subscriptionService;
 
     @PostMapping
-    public ResponseEntity<?> createSubscription(@Valid @RequestBody CreateSubscriptionRequestModel requestModel) {
-        return subscriptionService.createSubscription(requestModel);
+    public ResponseEntity<?> createSubscription(@Valid @RequestBody CreateSubscriptionRequestModel requestModel, BindingResult result) {
+        return result.hasErrors() ?
+                createResponseEntityForBindingResultErrors(result) :
+                subscriptionService.createSubscription(requestModel);
     }
 
     @GetMapping
@@ -40,7 +45,15 @@ public class SubscriptionController {
     }
 
     @PutMapping
-    public ResponseEntity<?> updateSubscription(@Valid @RequestBody UpdateSubscriptionRequestModel requestModel) {
-        return subscriptionService.updateSubscription(requestModel);
+    public ResponseEntity<?> updateSubscription(@Valid @RequestBody UpdateSubscriptionRequestModel requestModel, BindingResult result) {
+        return result.hasErrors() ?
+                createResponseEntityForBindingResultErrors(result) :
+                subscriptionService.updateSubscription(requestModel);
+    }
+
+    private ResponseEntity<String> createResponseEntityForBindingResultErrors(BindingResult result) {
+        List<String> errorMessages = result.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList());
+        Optional<String> errorMessage = errorMessages.stream().reduce((a, b)-> a + "\n" + b);
+        return new ResponseEntity<>(errorMessage.orElse("Validation failed on request body"), HttpStatus.BAD_REQUEST);
     }
 }
